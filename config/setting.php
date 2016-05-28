7c7c72447328b61230accec96c323fae33b65861
Jedsadha <Jedsadha1777@users.noreply.github.com>
2016-05-28 11:02:56 +0700

    Create setting.php

---
+++ config/setting.php
@@ -0,0 +1,10 @@
+<?php 
+
+/* Twitter */
+define('_oauth_access_token'           ,’TWITTER_ACCESS_TOKEN’);
+define('_oauth_access_token_secret'    ,‘TWITTER_ACCESS_TOKEN_SECRET’);
+define('_consumer_key'     ,‘CONSUMER_KEY’);
+define('_consumer_secret'  ,‘CUNSUMER_SECRET’);
+/* Twitter */
+
+?>
